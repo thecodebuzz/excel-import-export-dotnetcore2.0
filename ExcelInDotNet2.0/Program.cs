9b424bab118ca9e6d93f2fc3980f49faef302884
CodeBuzz <codebuzz.net@gmail.com>
2019-01-26 00:19:53 -0500

    Add project files.

---
+++ ExcelInDotNet2.0/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace ExcelInDotNet2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //open the excel using openxml sdk  
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open("testdata.xlsx", false))
+                {
+                    //create the object for workbook part  
+                    WorkbookPart workbookPart = doc.WorkbookPart;
+                    Sheets thesheetcollection = workbookPart.Workbook.GetFirstChild<Sheets>();
+                    StringBuilder excelResult = new StringBuilder();
+
+                    //using for each loop to get the sheet from the sheetcollection  
+                    foreach (Sheet thesheet in thesheetcollection)
+                    {
+                        excelResult.AppendLine("Excel Sheet Name : " + thesheet.Name);
+                        excelResult.AppendLine("----------------------------------------------- ");
+                        //statement to get the worksheet object by using the sheet id  
+                        Worksheet theWorksheet = ((WorksheetPart)workbookPart.GetPartById(thesheet.Id)).Worksheet;
+
+                        SheetData thesheetdata = (SheetData)theWorksheet.GetFirstChild<SheetData>();
+                        foreach (Row thecurrentrow in thesheetdata)
+                        {
+                            foreach (Cell thecurrentcell in thecurrentrow)
+                            {
+                                //statement to take the integer value  
+                                string currentcellvalue = string.Empty;
+                                if (thecurrentcell.DataType != null)
+                                {
+                                    if (thecurrentcell.DataType == CellValues.SharedString)
+                                    {
+                                        int id;
+                                        if (Int32.TryParse(thecurrentcell.InnerText, out id))
+                                        {
+                                            SharedStringItem item = workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(id);
+                                            if (item.Text != null)
+                                            {
+                                                //code to take the string value  
+                                                excelResult.Append(item.Text.Text + " ");
+                                            }
+                                            else if (item.InnerText != null)
+                                            {
+                                                currentcellvalue = item.InnerText;
+                                            }
+                                            else if (item.InnerXml != null)
+                                            {
+                                                currentcellvalue = item.InnerXml;
+                                            }
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    excelResult.Append(Convert.ToInt16(thecurrentcell.InnerText) + " ");
+                                }
+                            }
+                            excelResult.AppendLine();
+                        }
+                        excelResult.Append("");
+                        Console.WriteLine(excelResult.ToString());
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
