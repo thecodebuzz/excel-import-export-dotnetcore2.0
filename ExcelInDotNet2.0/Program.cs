dca8d676de0b62fa737ddc76d321585c181479f1
CodeBuzz <codebuzz.net@gmail.com>
2019-04-28 01:54:32 -0400

    commited missing file

--- ExcelInDotNet2.0/Program.cs
+++ ExcelInDotNet2.0/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
+using Newtonsoft.Json;
 
 namespace ExcelInDotNet2._0
 {
@@ -11,9 +15,17 @@ namespace ExcelInDotNet2._0
     {
         static void Main(string[] args)
         {
+
+            ReadExcelFile();
+            //WriteExcelFile();
+
+        }
+
+        static void ReadExcelFile()
+        {
             try
             {
-                //open the excel using openxml sdk  
+                //Lets open the existing excel file and read through its content . Open the excel using openxml sdk
                 using (SpreadsheetDocument doc = SpreadsheetDocument.Open("testdata.xlsx", false))
                 {
                     //create the object for workbook part  
@@ -78,5 +90,66 @@ namespace ExcelInDotNet2._0
 
             }
         }
+        static void WriteExcelFile()
+        {
+            List<UserDetails> persons = new List<UserDetails>()
+            {
+                new UserDetails() {ID="1001", Name="ABCD", City ="City1", Country="USA"},
+                new UserDetails() {ID="1002", Name="PQRS", City ="City2", Country="INDIA"},
+                new UserDetails() {ID="1003", Name="XYZZ", City ="City3", Country="CHINA"},
+                new UserDetails() {ID="1004", Name="LMNO", City ="City4", Country="UK"},
+           };
+
+            // Lets converts our object data to Datatable for a simplified logic.
+            // Datatable is most easy way to deal with complex datatypes for easy reading and formatting. 
+            DataTable table = (DataTable)JsonConvert.DeserializeObject(JsonConvert.SerializeObject(persons), (typeof(DataTable)));
+
+            using (SpreadsheetDocument document = SpreadsheetDocument.Create("TestNewData.xlsx", SpreadsheetDocumentType.Workbook))
+            {
+                WorkbookPart workbookPart = document.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                var sheetData = new SheetData();
+                worksheetPart.Worksheet = new Worksheet(sheetData);
+
+                Sheets sheets = workbookPart.Workbook.AppendChild(new Sheets());
+                Sheet sheet = new Sheet() { Id = workbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Sheet1" };
+
+                sheets.Append(sheet);
+
+                Row headerRow = new Row();
+
+                List<String> columns = new List<string>();
+                foreach (System.Data.DataColumn column in table.Columns)
+                {
+                    columns.Add(column.ColumnName);
+
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(column.ColumnName);
+                    headerRow.AppendChild(cell);
+                }
+
+                sheetData.AppendChild(headerRow);
+
+                foreach (DataRow dsrow in table.Rows)
+                {
+                    Row newRow = new Row();
+                    foreach (String col in columns)
+                    {
+                        Cell cell = new Cell();
+                        cell.DataType = CellValues.String;
+                        cell.CellValue = new CellValue(dsrow[col].ToString());
+                        newRow.AppendChild(cell);
+                    }
+
+                    sheetData.AppendChild(newRow);
+                }
+
+                workbookPart.Workbook.Save();
+            }
+        }
+
     }
 }
---
+++ ExcelInDotNet2.0/UserDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace ExcelInDotNet2._0
+{
+    internal class UserDetails
+    {
+        public string ID { get; internal set; }
+        public string Name { get; internal set; }
+        public string City { get; internal set; }
+        public string Country { get; internal set; }
+    }
+}
